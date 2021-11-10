3bc0d25c5b241baf5948833535fc816f62358d0d
testercwt <67111993+testercwt@users.noreply.github.com>
2021-11-10 11:43:56 +0800

    Update api.jl
    interpolation string directly, much faster

--- src/api.jl
+++ src/api.jl
@@ -4,12 +4,13 @@ using ..Telegram
 include("telegram_api.jl")
 
 for (f, fdoc) in TELEGRAM_API
+    fs=string(f)
     @eval begin
         function $f(client::Telegram.TelegramClient = Telegram.DEFAULT_OPTS.client; kwargs...)
             params = Dict{Symbol, Any}(kwargs)
             params[:chat_id] = get(params, :chat_id, Telegram.chatid(client))
             params[:parse_mode] = get(params, :parse_mode, client.parse_mode)
-            Telegram.query(client, String(Symbol($f)), params = params)
+            Telegram.query(client, $fs, params = params) # interpolation string directly, much faster
         end
 
         @doc $fdoc $f
